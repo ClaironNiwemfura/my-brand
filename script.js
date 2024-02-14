b608c391954324db89927eff907bcaa55ad4a31b
ClaironNiwemfura <niwemfurac@gmail.com>
2024-02-14 12:08:34 -0800

    adding responsiveness for navigation bar

--- script.js
+++ script.js
@@ -1,6 +1,15 @@
-function toggleMenu(){
-const menu=document.querySelector(".menu-links");
-const icon = document.querySelector(".hamburger-icon")
-menu.classlist.toggle("open");
-icon.classlist.toggle("open")
-}
+// function toggleMenu(){
+// const menu=document.querySelector(".menu-links");
+// const icon = document.querySelector(".hamburger-icon")
+// menu.classlist.toggle("open");
+// icon.classlist.toggle("open")
+// }
+
+// window.onload= function () {
+//     const menu_btn =document.querySelector('.hamburger')
+//     const mobile_menu= document.querySelector('.mobile-nav')
+//     menu_btn.addEventListener('click', function() {
+//         menu_btn.classList.toggle('is-active');
+//         mobile_menu.classList.toggle('is-active')
+//     })
+// }
