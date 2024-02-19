50d36b7351d9c514180a399de4da0c06adfc76d5
ClaironNiwemfura <niwemfurac@gmail.com>
2024-02-19 15:02:21 -0800

    form validation and storing on local storage for contact page

--- scripts/login.js
+++ scripts/login.js
@@ -4,26 +4,41 @@ loginForm.addEventListener('submit', function(e) {
     e.preventDefault();
     validateLogin();
 });
+const setError = (element, message) => {
+    const inputControl = element.parentElement;
+    const errorDisplay = inputControl.querySelector('.login-error');
+    errorDisplay.innerText = message;
+    inputControl.classList.add('error');
+    inputControl.classList.remove('success');
+}
 
+const setSuccess = element => {
+    const inputControl = element.parentElement;
+    const errorDisplay = inputControl.querySelector('.login-error');
+    errorDisplay.innerText = '';
+    inputControl.classList.add('success');
+    inputControl.classList.remove('error');
+}
 function validateLogin() {
     const emailValue1 = document.getElementById("email1").value.trim();
     const passwordValue1 = document.getElementById("password1").value.trim();
 
     if (emailValue1 === '') {
-        setError(email, 'Email is required');
+        setError(email1, 'Email is required');
         return;
     }
     if (passwordValue1 === '') {
-        setError(password, "Password is not available")
+        setError(password1, "Password is not available")
     }
     let userData = JSON.parse(localStorage.getItem("usersData")) || [];
-
+    if (passwordValue1 !== userData.password) {
+        setError(password1, "incorrect Password ")
+    }
     const user = userData.find(u => u.email === emailValue1 && u.password === passwordValue1);
     if (user) {
         window.location.href = "blog.html";
     } else {
-
-        const errorMessage = document.getElementById("login-error");
+        const errorMessage = document.getElementById("login-error2");
         errorMessage.textContent = "Invalid email or password";
     }
 }
